ef5512827e7aa85baf03dbc9a9aac23d23ced361
LHenriquerios <henriquerios4@gmail.com>
2022-07-13 15:33:04 -0300

    feat: add route get all macthes

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import LoginController from '../controllers/Login';
 import TeamController from '../controllers/Teams';
+import MatchesController from '../controllers/Matches';
 import validateJoi from '../middlewares/validateJoi';
 import Schema from '../schemas';
 import authToken from '../middlewares/authToken';
@@ -8,10 +9,12 @@ import authToken from '../middlewares/authToken';
 const router = express.Router();
 const loginController = new LoginController();
 const teamController = new TeamController();
+const matchesController = new MatchesController();
 
 router.post('/login', validateJoi(Schema), loginController.create);
 router.get('/login/validate', authToken, loginController.getValidade);
 router.get('/teams', teamController.getAll);
 router.get('/teams/:id', teamController.getById);
+router.get('/matches', matchesController.getAll);
 
 export default router;
