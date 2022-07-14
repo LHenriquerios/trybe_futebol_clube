8762e27b2052c9c0f69164a9ef78363effe541d8
LHenriquerios <henriquerios4@gmail.com>
2022-07-13 21:46:41 -0300

    feat: auth token when creating matches

--- app/backend/src/routes/matchesRouter/index.ts
+++ app/backend/src/routes/matchesRouter/index.ts
@@ -1,11 +1,12 @@
 import * as express from 'express';
+import authToken from '../../middlewares/authToken';
 import MatchController from '../../controllers/Matches';
 
 const matchRouter = express.Router();
 const matchController = new MatchController();
 
 matchRouter.get('/matches', matchController.getAll);
-matchRouter.post('/matches', matchController.createMacth);
+matchRouter.post('/matches', authToken, matchController.createMacth);
 matchRouter.patch('/matches/:id', matchController.updateMacth);
 matchRouter.patch('/matches/:id/finish', matchController.finishMacth);
 
