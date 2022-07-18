54c5720a721a688bf670d96e175e9b20a84770e6
LHenriquerios <henriquerios4@gmail.com>
2022-07-18 17:17:35 -0300

    fix: route url

--- app/backend/src/routes/leaderboardRouter/index.ts
+++ app/backend/src/routes/leaderboardRouter/index.ts
@@ -4,6 +4,6 @@ import LeaderboardController from '../../controllers/leaderboard';
 const leaderboardRouter = express.Router();
 const leaderboardController = new LeaderboardController();
 
-leaderboardRouter.get('/leaderboardes/home', leaderboardController.getAll);
+leaderboardRouter.get('/leaderboard/home', leaderboardController.getAll);
 
 export default leaderboardRouter;
